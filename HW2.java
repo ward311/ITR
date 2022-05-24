415581b1b5572c340b9070f66c22c2cf060a7afb
Sh4dy <marshall871018@gmail.com>
2022-05-24 17:32:38 +0800

    update code and ppt

--- HW2.java
+++ HW2.java
@@ -1,9 +1,6 @@
 //M104020014 周紘樟
 //TODO: student id & name
 //TODO: This assignment aims to write a function simulate to allocate the course process by student’s preference.
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
 import javax.swing.text.Style;
 
 import java.nio.charset.CoderResult;
@@ -36,12 +33,14 @@ public class HW2 {
         //A set of student's preferences of courses id. e.g. [4,3,2,1]. The first priority of course is 4, which means ResearchMethod
     int[] preference;
     int round = 0; //individuals preference array index 
+    boolean hasChosen; // check if the students already have courses.
     // Student should initial by year, id ,and candidate_courses.
     private Student(int year, int id, int[] preference) {
         this.year = year;
         this.preference = preference;
         this.id = id;
         this.round = 0; //initialize variable
+        this.hasChosen = false;//initialize variable
     }
     
     //overriding the toString() method
@@ -116,6 +115,36 @@ public class HW2 {
   // Abstract test case 3. Prepare for hidden test case.
   // It is normal that the function is no code.
   private static void test3() {
+    Course[] courses = new Course[4];
+    courses[0] = new Course(1, "ITR", 3);
+    courses[1] = new Course(2, "MIS", 3);
+    courses[2] = new Course(3, "DataBase", 3);
+    courses[3] = new Course(4, "ResearchMethod", 3);
+    
+    
+    
+    Student[] students = new Student[12];
+    students[0] =  new Student(3, 22, new int[]{1, 2, 3});
+    students[1] =  new Student(4, 122, new int[]{1, 2, 3});
+    students[2] =  new Student(2, 1, new int[]{1, 2, 3});
+    students[3] =  new Student(3, 23, new int[]{1, 2, 3});
+    students[4] =  new Student(1, 6, new int[]{1, 2});
+    students[5] =  new Student(3, 21, new int[]{2, 1, 3});
+    students[6] =  new Student(3, 31, new int[]{1, 2, 3});
+    students[7] =  new Student(2, 12, new int[]{1, 2, 3});
+    students[8] =  new Student(3, 33, new int[]{1, 2, 3});
+    students[9] =  new Student(4, 1, new int[]{});
+    students[10] =  new Student(3, 3, new int[]{1, 2, 3});
+    students[11] =  new Student(1, 43, new int[]{1, 2, 3});
+    
+    
+    System.out.println("Test case3:");
+    long startTime = System.nanoTime();
+    Course[] result = simulate(students, courses);
+    long estimatedTime = System.nanoTime() - startTime;
+    print_course(result);
+    print_first_priority(result);
+    System.out.println("Performance(time): "+estimatedTime);
   }
   
   // TODO: write your code in this function
@@ -123,8 +152,7 @@ public class HW2 {
   private static Course[] simulate(Student[] students, Course[] courses) {
     ArrayList<Student> sortList = new ArrayList<>(); //ArrayList for students with preference to sort
     ArrayList<Student> blankList = new ArrayList<>();//ArrayList for students with no preference
-
-    for(Student student : students){  //for loop go through sutdents' data
+      for(Student student : students){  //for loop go through sutdents' data
       if(student.preference.length==0) {  //if student doesn't have any preference
         blankList.add(student);           //add into blankList
         continue;                         //continue the loop looking for next one
@@ -152,9 +180,9 @@ public class HW2 {
 
     sortList.addAll(blankList); //Finally, We concatenate two sorted arraylists 
                                 //which students with no preferences stay at the end of the arrayList
-
+                                
     //System.out.println("Priority for Blank after Sorted: "+blankList);
-    //System.out.println("Priority after Sorted with Blank: "+sortList);
+    System.out.println("Priority after Sorted with Blank: "+sortList);
     //System.out.println("No preference chosen: "+blankList);
   
     
@@ -166,6 +194,7 @@ public class HW2 {
             //if the first course they choose is still available
             courses[sortList.get(j).preference[i]-1].candidate[courses[sortList.get(j).preference[i]-1].available++] = sortList.get(j);
             //we successfully add that student to the course
+            sortList.get(j).hasChosen = true; //we make sure the students get the course
             break in;
             //break inner loop, go to the next student
           }else{
@@ -173,17 +202,30 @@ public class HW2 {
           }
 
         }
+        if(sortList.get(j).hasChosen == false){ 
+          // if the all preferred-courses of current student was not available, 
+          //then we loop through the existing courses to check if there are still available spots
+          for(int k = 0;k<courses.length;k++){//then we loop through the existing courses to check if there are still available spots
+            if(courses[k].available < courses[k].candidate.length){//if we find spots for students with no preference
+              courses[k].candidate[courses[k].available++] = sortList.get(j);//add them into the spot, which takes one more space
+              sortList.get(j).hasChosen = true;//we make sure the students get the course
+              break; //break the loop as we successfully add
+            }else continue; //no available spot, keep finding the next course
+          }
+        }
         
       }else{//students with no preference
         for(int k = 0;k<courses.length;k++){//then we loop through the existing courses to check if there are still available spots
           if(courses[k].available < courses[k].candidate.length){//if we find spots for students with no preference
             courses[k].candidate[courses[k].available++] = sortList.get(j);//add them into the spot, which takes one more space
+            sortList.get(j).hasChosen = true;//we make sure the students get the course
             break; //break the loop as we successfully add
           }else continue; //no available spot, keep finding the next course
         }
       }
       
-    } 
+    }
+    
 
     return courses;
   }
